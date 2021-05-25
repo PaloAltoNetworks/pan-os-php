5394a502b1a652417f0277152063c3b91e75f833
Sven Waschkut <sven@waschkut.de>
2021-05-25 23:21:34 +0200

    class PanAPIConnector - extend $apiTimeOut value usage

--- lib/misc-classes/PanAPIConnector.php
+++ lib/misc-classes/PanAPIConnector.php
@@ -1354,11 +1354,12 @@ class PanAPIConnector
         return $r;
     }
 
-    public function getPanoramaPushedConfig()
+    public function getPanoramaPushedConfig( $apiTimeOut = 30 )
     {
         $url = 'action=get&type=config&xpath=/config/panorama';
-
-        $r = $this->sendRequest($url, TRUE);
+        $moreOptions = array('timeout' => $apiTimeOut, 'lowSpeedTime' => 0);
+        $filecontent = null;
+        $r = $this->sendRequest($url, TRUE, $filecontent, '', $moreOptions);
 
         $configRoot = DH::findFirstElement('result', $r);
         if( $configRoot === FALSE )
@@ -1583,13 +1584,14 @@ class PanAPIConnector
      * @param $maxWaitTime integer
      * @return DomDocument|string[]
      */
-    public function sendCmdRequest($cmd, $checkResultTag = TRUE, $maxWaitTime = -1)
+    public function sendCmdRequest($cmd, $checkResultTag = TRUE, $maxWaitTime = -1, $apiTimeOut = 7)
     {
         $req = "type=op&cmd=$cmd";
         if( $maxWaitTime == -1 )
             $moreOptions['lowSpeedTime'] = null;
         else
             $moreOptions['lowSpeedTime'] = $maxWaitTime;
+        $moreOptions['timeout'] = $apiTimeOut;
 
         $nullVar = null;
 
@@ -1598,10 +1600,12 @@ class PanAPIConnector
         return $ret;
     }
 
-    public function getJobResult($jobID)
+    public function getJobResult($jobID, $apiTimeOut = 7)
     {
         $req = "type=op&cmd=<show><jobs><id>$jobID</id></jobs></show>";
-        $ret = $this->sendRequest($req);
+        $moreOptions['timeout'] = $apiTimeOut;
+        $filecontent = null;
+        $ret = $this->sendRequest($req, false, $filecontent, '', $moreOptions);
 
         //TODO: 20180305 not working
         $found = &searchForName('name', 'result', $ret);
@@ -1660,14 +1664,14 @@ class PanAPIConnector
      * @param bool $verbose
      * @return DOMNode
      */
-    public function uploadConfiguration($configDomXml, $configName = 'stage0.xml', $verbose = TRUE)
+    public function uploadConfiguration($configDomXml, $configName = 'stage0.xml', $verbose = TRUE, $apiTimeOut = 7)
     {
         if( $verbose )
             print "Uploadig config to device {$this->apihost}/{$configName}....";
 
         $url = "type=import&category=configuration&category=configuration";
 
-        $answer = $this->sendRequest($url, FALSE, DH::dom_to_xml($configDomXml), $configName, array('timeout' => 7));
+        $answer = $this->sendRequest($url, FALSE, DH::dom_to_xml($configDomXml), $configName, array('timeout' => $apiTimeOut));
 
         if( $verbose )
             print "OK!\n";
