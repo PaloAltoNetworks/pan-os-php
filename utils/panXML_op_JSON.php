072b701721d7d55c557fbf78dc5d0f47d8460122
Sven Waschkut <sven@waschkut.de>
2021-08-05 10:26:04 +0200

    UTIL script pa_xml-op-json - extend with argument 'cycleconnectedfirewalls'

--- utils/panXML_op_JSON.php
+++ utils/panXML_op_JSON.php
@@ -34,6 +34,7 @@ $supportedArguments['cmd'] = array('niceName' => 'cmd', 'shortHelp' => 'PAN-OS X
 $supportedArguments['debugapi'] = array('niceName' => 'DebugAPI', 'shortHelp' => 'prints API calls when they happen');
 $supportedArguments['shadow-json'] = array('niceName' => 'shadow-JSON', 'shortHelp' => 'print out ONLY JSON string');
 $supportedArguments['help'] = array('niceName' => 'help', 'shortHelp' => 'this message');
+$supportedArguments['cycleconnectedfirewalls'] = array('niceName' => 'cycleConnectedFirewalls', 'shortHelp' => 'a listing of all devices connected to Panorama will be collected through API then each firewall will be queried for bpa generator');
 
 
 $usageMsg = PH::boldText('USAGE: ') . "php " . basename(__FILE__) . " in=api://[MGMT-IP] [cycleconnectedFirewalls] [actions=enable]";
@@ -59,18 +60,51 @@ $configInput = $util->configInput;
 
 $connector = $util->pan->connector;
 
+if( isset(PH::$args['cycleconnectedfirewalls']) )
+    $cycleConnectedFirewalls = TRUE;
+else
+    $cycleConnectedFirewalls = FALSE;
+
 if( isset(PH::$args['cmd'] ) )
 {
     $cmd = PH::$args['cmd'];
 
     if( $configInput['type'] == 'api' )
     {
-        $response = $connector->sendOpRequest($cmd, FALSE);
+        if( $cycleConnectedFirewalls && $configType == 'panorama' )
+        {
+            $xml_string = "";
 
-        $response->preserveWhiteSpace = false;
-        $response->formatOutput = true;
+            $firewallSerials = $connector->panorama_getConnectedFirewallsSerials();
 
-        $xml_string = $response->saveXML($response->documentElement);
+            $countFW = 0;
+            foreach( $firewallSerials as $fw )
+            {
+                $countFW++;
+                print " ** Handling FW #{$countFW}/" . count($firewallSerials) . " : serial/{$fw['serial']}   hostname/{$fw['hostname']} **\n";
+                $tmpConnector = $inputConnector->cloneForPanoramaManagedDevice($fw['serial']);
+
+                if( $util->debugAPI )
+                    $tmpConnector->setShowApiCalls(TRUE);
+
+                $response = $tmpConnector->sendOpRequest($cmd, FALSE);
+
+                $response->preserveWhiteSpace = false;
+                $response->formatOutput = true;
+
+                $xml_string .= $response->saveXML($response->documentElement);
+            }
+        }
+        else
+        {
+            $response = $connector->sendOpRequest($cmd, FALSE);
+
+            $response->preserveWhiteSpace = false;
+            $response->formatOutput = true;
+
+            $xml_string = $response->saveXML($response->documentElement);
+        }
+
     }
     else
     {
