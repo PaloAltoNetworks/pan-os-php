f411b8527f9919c646609a505b7557ce2b8bf5bd
Sven Waschkut <sven@waschkut.de>
2021-09-03 11:33:37 +0200

    Framework - update to version 2.0.10

--- lib/misc-classes/PH.php
+++ lib/misc-classes/PH.php
@@ -140,7 +140,7 @@ class PH
 
     private static $library_version_major = 2;
     private static $library_version_sub = 0;
-    private static $library_version_bugfix = 9;
+    private static $library_version_bugfix = 10;
 
     //BASIC AUTH PAN-OS 7.1
     public static $softwareupdate_key = "658d787f293e631196dac9fb29490f1cc1bb3827";
