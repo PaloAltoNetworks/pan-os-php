feb4388ee318dd218195a7d470af97ce089c1956
Sven Waschkut <sven@waschkut.de>
2021-09-29 15:25:57 +0200

    framework - optimise to set alias information

--- mac_set_path_variable.sh
+++ mac_set_path_variable.sh
@@ -2,24 +2,41 @@
 
 FOLDER_PATH=$PWD
 
-echo "START \"set path variables\"" \
+
+#echo "START \"set path variables\"" \
+#&& echo "check if everything is successfully installed" \
+#&& php -r "require('lib/pan_php_framework.php');print \"PAN-OS-PHP LIBRARY - OK INSTALL SUCCESSFUL\n\";" \
+#&& echo "" \
+#&& echo "" \
+#&& echo "" \
+#&& echo "" \
+#&& echo "prepare php.ini and set include path" \
+#&& sudo cp -f /etc/php.ini.default /etc/php.ini \
+#&& sudo chmod u+w /etc/php.ini \
+
+
+check="source ${FOLDER_PATH}/utils/alias.sh"
+
+
+echo "START \"set user bash profile\""   \
 && echo "" \
 && php -v \
 && echo "" \
-&& echo "prepare php.ini and set include path" \
-&& sudo cp -f /etc/php.ini.default /etc/php.ini \
-&& sudo chmod u+w /etc/php.ini \
-&& echo "include_path = \".:/php/includes:${FOLDER_PATH}\"" | sudo tee -a /etc/php.ini \
-&& echo "" \
-&& echo "" \
-&& echo "check if everything is successfully installed" \
-&& php -r "require('lib/pan_php_framework.php');print \"PAN-OS-PHP LIBRARY - OK INSTALL SUCCESSFUL\n\";" \
-&& echo "" \
-&& echo "" \
-&& echo "set user bash profile"   \
-&& echo "source \"${FOLDER_PATH}/utils/alias.sh\"" >> ~/.profile \
-&& echo "source \"${FOLDER_PATH}/utils/alias.sh\"" >> ~/.bash_profile \
-&& echo "source \"${FOLDER_PATH}/utils/alias.sh\"" >> ~/.zshrc \
-&& echo "" \
-&& echo "" \
+&& echo "set $check" \
+&& echo ""
+
+array=( ".profile" ".bash_profile" ".zshrc" )
+
+for filecheck in "${array[@]}"
+do
+	if grep -Fxq "$check" "$HOME/$filecheck"
+  then
+    echo "already available in: $filecheck"
+  else
+    echo $check >> ~/$filecheck \
+    && echo "set in: $filecheck"
+  fi
+done
+
+echo "" \
 && echo "END script"
