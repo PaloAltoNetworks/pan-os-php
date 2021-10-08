f4acc5a73c6335ffb8e3828cd07f6b4479e46dfa
Sven Waschkut <sven@waschkut.de>
2021-10-08 11:00:53 +0200

    UTIL - API & UI | improvement

---
+++ utils/develop/api/v1/file_upload.php
@@ -0,0 +1,49 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+$response = array();
+$upload_dir = '';
+$server_url = 'http://127.0.0.1:8082';
+if($_FILES['configInput'])
+{
+    $avatar_name = $_FILES["configInput"]["name"];
+    $avatar_tmp_name = $_FILES["configInput"]["tmp_name"];
+    $error = $_FILES["configInput"]["error"];
+    if($error > 0){
+        $response = array(
+            "status" => "error",
+            "error" => true,
+            "message" => "Error uploading the file!"
+        );
+    }else
+    {
+        $random_name = rand(1000,1000000)."-".$avatar_name;
+        $upload_name = $upload_dir.strtolower($random_name);
+        $upload_name = preg_replace('/\s+/', '-', $upload_name);
+        if(move_uploaded_file($avatar_tmp_name , $upload_name)) {
+            $response = array(
+                "status" => "success",
+                "error" => false,
+                "message" => "File uploaded successfully",
+                "url" => $server_url."/".$upload_name
+            );
+        }else
+        {
+            $response = array(
+                "status" => "error",
+                "error" => true,
+                "message" => "Error uploading the file!"
+            );
+        }
+    }
+}else{
+    $response = array(
+        "status" => "error",
+        "error" => true,
+        "message" => "No file was sent!"
+    );
+}
+echo json_encode($response);
+?>
+
