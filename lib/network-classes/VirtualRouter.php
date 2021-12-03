539868a3b54e6fd1bfbaa88544121ed771b47299
Sven Waschkut <sven@waschkut.de>
2021-12-03 12:03:26 +0100

    License / copyright change - based on a first deeper investigation by Palo Alto Networks Legal department - further investigation triggered by Legal
    2014:
    https://github.com/cpainchaud/pan-configurator/blob/dab5ea4e383167f924cd485b5d1ab7588377bcc3/LICENSE.txt
    2015:
    https://github.com/cpainchaud/pan-configurator/blob/1ae810bd09a9c929204982c95e8126b171f780a0/LICENSE.txt
    2017:
    https://github.com/cpainchaud/pan-configurator/commit/1fc43e23d93d1a6dae680023b1d4a06f6d11a31b#diff-d0ed4cc3fb70489fe51c7e0ac180cba2a7472124f9f9e9ae67b01a37fbd580b7
    2018:
    https://github.com/cpainchaud/pan-configurator/commit/39b00e19ace28e04d0f0175b50ef0801f31546c9#diff-d0ed4cc3fb70489fe51c7e0ac180cba2a7472124f9f9e9ae67b01a37fbd580b7

--- lib/network-classes/VirtualRouter.php
+++ lib/network-classes/VirtualRouter.php
@@ -2,7 +2,8 @@
 /**
  * ISC License
  *
- * Copyright (c) 2014-2018 Christophe Painchaud <shellescape _AT_ gmail.com>
+  * Copyright (c) 2014-2016, Palo Alto Networks Inc.
+ * Copyright (c) 2017-2018 Christophe Painchaud <shellescape _AT_ gmail.com>
  * Copyright (c) 2019, Palo Alto Networks Inc.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
