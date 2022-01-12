938a4c851cb7e4abdd3973260877562cce70349a
Sven Waschkut <sven@waschkut.de>
2022-01-12 23:46:02 +0100

    UTIL util_get-action-filter | move to php class

--- utils/util_get-action-filter.php
+++ utils/util_get-action-filter.php
@@ -22,29 +22,7 @@
 set_include_path(dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path());
 require_once dirname(__FILE__)."/../lib/pan_php_framework.php";
 require_once dirname(__FILE__)."/../utils/lib/UTIL.php";
+require_once dirname(__FILE__)."/../utils/lib/UTIL_GET_ACTION_FILTER.php";
 
 
-
-$util = new UTIL("custom", $argv, $argc, __FILE__);
-
-
-$array = array( 'address', 'service', 'tag', 'rule', 'zone', 'securityprofile', 'schedule','virtualwire','routing','interface','device', 'securityprofilegroup', 'application', 'threat');
-
-
-
-foreach( $array as $entry )
-{
-    $util->utilType = $entry;
-    $tmp_array[ $entry ]['action'] = $util ->supportedActions();
-
-    $filter_array = RQuery::$defaultFilters[$util->utilType];
-    ksort( $filter_array );
-    $tmp_array[ $entry ]['filter'] = $filter_array;
-}
-
-
-$JSON_pretty =  json_encode( $tmp_array, JSON_PRETTY_PRINT );
-
-print $JSON_pretty;
-
-file_put_contents(__DIR__ . "/lib/"."util_action_filter.json", $JSON_pretty);
+$test = new UTIL_GET_ACTION_FILTER( $argv, $argc );
