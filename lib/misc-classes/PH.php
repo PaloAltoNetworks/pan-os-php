a5cfb502d8aff652bec7e667a0c35253c65f3f7a
Sven Waschkut <sven@waschkut.de>
2022-04-19 11:25:11 +0200

    framework - start version 2.0.37

--- lib/misc-classes/PH.php
+++ lib/misc-classes/PH.php
@@ -153,7 +153,7 @@ class PH
 
     private static $library_version_major = 2;
     private static $library_version_sub = 0;
-    private static $library_version_bugfix = 36;
+    private static $library_version_bugfix = 37;
 
     //BASIC AUTH PAN-OS 7.1
     public static $softwareupdate_key = "658d787f293e631196dac9fb29490f1cc1bb3827";
