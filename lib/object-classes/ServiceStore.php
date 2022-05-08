cf1c87a1721967f7604351d337f08255df407b0a
Sven Waschkut <sven@waschkut.de>
2022-05-08 23:09:51 +0200

    UTIL ServiceStore | bugfix if serviceGroup A has a ServiceGroup B as member, which contains ServiceGroupA

--- lib/object-classes/ServiceStore.php
+++ lib/object-classes/ServiceStore.php
@@ -157,7 +157,7 @@ class ServiceStore
      * @return ServiceGroup[]
      * @var bool $sortByDependencies
      */
-    public function serviceGroups($sortByDependencies = TRUE)
+    public function serviceGroups($sortByDependencies = FALSE)
     {
         if( !$sortByDependencies )
             return $this->_serviceGroups;
@@ -191,6 +191,16 @@ class ServiceStore
                 if( count($groupDependencies) == 0 )
                 {
                     $result[] = $this->_serviceGroups[$groupName];
+                    unset($sortingArray[$groupName]);
+
+                    foreach( $sortingArray as &$tmpGroupDeps )
+                    {
+                        if( isset($tmpGroupDeps[$groupName]) )
+                            unset($tmpGroupDeps[$groupName]);
+                    }
+                }
+                elseif( count($groupDependencies) == 1 )
+                {
                     unset($sortingArray[$groupName]);
 
                     foreach( $sortingArray as &$tmpGroupDeps )
