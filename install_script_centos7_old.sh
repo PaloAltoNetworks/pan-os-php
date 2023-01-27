3124482be13789ce1d65dfbfbabd6b01173cc45e
Sven Waschkut <swaschkut@paloaltonetworks.com>
2023-01-27 01:23:10 +0100

    update Dockerfiles related to ironskillet yaml usage

--- install_script_centos7_old.sh
+++ install_script_centos7_old.sh
@@ -21,7 +21,7 @@ echo "START \"install PAN-OS-PHP on CENTOS\"" \
 && yum-config-manager --enable remi-php56 \
 && echo "" \
 && echo "" \
-&& yum -y install php php-cli php-fpm php-json php-intl php-curl php-dom php-mbstring php-bcmath && yum clean all \
+&& yum -y install php php-cli php-fpm php-json php-intl php-curl php-dom php-mbstring php-bcmath php-yaml && yum clean all \
 && php -v \
 && echo "" \
 && echo "" \
