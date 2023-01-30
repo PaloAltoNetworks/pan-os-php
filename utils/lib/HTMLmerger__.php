5cd87566e0f1415e96ff79ea0aa0262f6e5816b1
Sven Waschkut <swaschkut@paloaltonetworks.com>
2023-01-30 09:42:54 +0100

    Update HTMLmerger__.php

--- utils/lib/HTMLmerger__.php
+++ utils/lib/HTMLmerger__.php
@@ -80,14 +80,23 @@ class HTMLmerger__
         if( isset(PH::$args['adddefaulthtml']) )
         {
             if( empty(PH::$args['adddefaulthtml']) || PH::$args['adddefaulthtml'] == "adddefaulthtml" )
-                $defaultfilename = dirname(__FILE__) . '/../common/html/Introduction.html';
+            {
+                $defaultfile = dirname(__FILE__) . '/../common/html/Introduction.html';
+                $defaultfilenname = "0_Introduction.html";
+            }
             else
-                $defaultfilename = PH::$args['adddefaulthtml'];
-            //copy default html file into projectfolder;
+            {
+                //get own defaultfile from anywhere outside container/panosphp environment
+                $defaultfile = PH::$args['adddefaulthtml'];
+                $pathArray = explode("/", $defaultfile);
+                $tmpname = end( $pathArray );
+                $defaultfilenname = "0_".$tmpname;
+            }
 
-            $projectdefaultfile = file_get_contents( $defaultfilename );
+            //copy default html file into projectfolder;
+            $projectdefaultfile = file_get_contents( $defaultfile );
             #bug how to get only file name if $defaultfilename is specified
-            file_put_contents($this->projectfolder . "/0_Introduction.html", $projectdefaultfile);
+            file_put_contents($this->projectfolder . "/".$defaultfilenname, $projectdefaultfile);
         }
 
         /*
