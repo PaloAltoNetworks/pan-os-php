2b672b610ce060ed79e7913c1cf8edad01c5c5c6
Sven Waschkut <swaschkut@paloaltonetworks.com>
2023-02-03 10:23:01 +0100

    introduce v2.0.70 - move docker arm creation to testing mode

--- create_all_docker_container.sh
+++ create_all_docker_container.sh
@@ -9,4 +9,6 @@ echo "create all \"PAN-OS-PHP docker container\" - pan-os-php/-cli/-api" \
 && docker build -t swaschkut/pan-os-php-cli:${TAGNAME} -f docker/Dockerfile-php . \
 && echo "" \
 && docker build -t swaschkut/pan-os-php-api:${TAGNAME} -f docker/Dockerfile-API . \
+&& echo "" \
+&& docker build -t swaschkut/pan-os-php-arm:${TAGNAME} -f docker/Dockerfile_arm64v8_full . \
 && echo ""
