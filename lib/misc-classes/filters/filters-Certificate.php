64c646485fdc4195732d93f7f6a2683859d3df66
Sven Waschkut <swaschkut@paloaltonetworks.com>
2023-02-20 09:11:04 +0100

    type=certificate | introduce 'filter=(publickey-hash < sha256)'

--- lib/misc-classes/filters/filters-Certificate.php
+++ lib/misc-classes/filters/filters-Certificate.php
@@ -97,7 +97,40 @@ RQuery::$defaultFilters['certificate']['publickey-hash']['operators']['is.sha512
         'input' => 'input/panorama-8.0.xml'
     )
 );
+RQuery::$defaultFilters['certificate']['publickey-hash']['operators']['>,<,=,!'] = array(
+    'Function' => function (CertificateRQueryContext $context) {
+        $object = $context->object;
+        $arg = $context->value;
 
+        $operator = $context->operator;
+        if( $operator == '=' )
+            $operator = '==';
+
+        if( !$context->object->hasPublicKey() )
+            return FALSE;
+
+        $hashvalue = $context->object->getPkeyHash();
+        $hashvalue = str_replace( "sha", "", $hashvalue );
+
+        $hashvaluesearch = $arg;
+        if( strpos( $hashvaluesearch, "sha" ) !== FALSE )
+        {
+            $hashvaluesearch = str_replace( "sha", "", $hashvaluesearch );
+        }
+
+        $operator_string = $hashvalue." ".$operator." ".$hashvaluesearch;
+        if( eval("return $operator_string;" ) )
+            return TRUE;
+
+        return FALSE;
+    },
+    'arg' => true,
+    'help' => 'returns TRUE if object hash value matches "publickey-hash >= 256" || "publickey-hash < sha256"',
+    'ci' => array(
+    'fString' => '(%PROP% 5)',
+    'input' => 'input/panorama-8.0.xml'
+)
+);
 RQuery::$defaultFilters['certificate']['publickey-length']['operators']['>,<,=,!'] = array(
     'eval' => '$object->hasPublicKey() && $object->getPkeyBits() !operator! !value!',
     'arg' => TRUE,
