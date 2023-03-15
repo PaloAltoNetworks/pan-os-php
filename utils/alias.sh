1f6caa9e95c33567a4cdab21d135838cf5898be8
Sven Waschkut <swaschkut@paloaltonetworks.com>
2023-03-15 08:54:13 +0100

    cleanup alias.sh - remove deprecated util scripts which are directly supported with alias "pan-os-php type=XYZ"

--- utils/alias.sh
+++ utils/alias.sh
@@ -10,83 +10,10 @@ alias pa_docker-panosphp='docker run --name panosphp --rm -v $PWD:/share -it swa
 
 
 
-
 alias pan-os-php='php -r "require_once '"'"'utils/pan-os-php.php'"'"';" $@'
 ##########################################################################################
 ##########################################################################################
 ##########################################################################################
-# the following scripts are deprecated and supported by pan-os-php
-alias pa_rule-edit='php -r "require_once '"'"'utils/rules-edit.php'"'"';" $@'
-alias pa_rule-stats='php -r "require_once '"'"'utils/rules-stats.php'"'"';" $@'
-
-alias pa_address-edit='php -r "require_once '"'"'utils/address-edit.php'"'"';" $@'
-alias pa_service-edit='php -r "require_once '"'"'utils/service-edit.php'"'"';" $@'
-alias pa_tag-edit='php -r "require_once '"'"'utils/tag-edit.php'"'"';" $@'
-
-alias pa_schedule-edit='php -r "require_once '"'"'utils/schedule-edit.php'"'"';" $@'
-alias pa_application-edit='php -r "require_once '"'"'utils/application-edit.php'"'"';" $@'
-
-alias pa_securityprofile-edit='php -r "require_once '"'"'utils/securityprofile-edit.php'"'"';" $@'
-alias pa_securityprofilegroup-edit='php -r "require_once '"'"'utils/securityprofilegroup-edit.php'"'"';" $@'
-
-alias pa_device-edit='php -r "require_once '"'"'utils/device-edit.php'"'"';" $@'
-alias pa_zone-edit='php -r "require_once '"'"'utils/zone-edit.php'"'"';" $@'
-alias pa_interface-edit='php -r "require_once '"'"'utils/interface-edit.php'"'"';" $@'
-alias pa_routing-edit='php -r "require_once '"'"'utils/routing-edit.php'"'"';" $@'
-alias pa_vwire-edit='php -r "require_once '"'"'utils/vwire-edit.php'"'"';" $@'
-
-alias pa_threat='php -r "require_once '"'"'utils/threat.php'"'"';" $@'
-##########################################################################################
-
-
-# MERGER class for all scripts
-alias pa_rule-merger='php -r "require_once '"'"'utils/rule-merger.php'"'"';" $@'
-alias pa_address-merger='php -r "require_once '"'"'utils/address-merger.php'"'"';" $@'
-alias pa_addressgroup-merger='php -r "require_once '"'"'utils/addressgroup-merger.php'"'"';" $@'
-alias pa_service-merger='php -r "require_once '"'"'utils/service-merger.php'"'"';" $@'
-alias pa_servicegroup-merger='php -r "require_once '"'"'utils/servicegroup-merger.php'"'"';" $@'
-alias pa_tag-merger='php -r "require_once '"'"'utils/tag-merger.php'"'"';" $@'
-
-
-##########################################################################################
-# scripts for optimisation - reduce duplicate code and using UTIL class
-
-alias pa_upload-config='php -r "require_once '"'"'utils/upload-config.php'"'"';" $@'
-alias pa_override-finder='php -r "require_once '"'"'utils/override-finder.php'"'"';" $@'
-alias pa_userid-mgr='php -r "require_once '"'"'utils/userid-mgr.php'"'"';" $@'
-alias pa_register-ip-mgr='php -r "require_once '"'"'utils/register-ip-mgr.php'"'"';" $@'
-
-alias pa_key-manager='php -r "require_once '"'"'utils/key-manager.php'"'"';" $@'
-
-
-
-alias pa_download-predefined='php -r "require_once '"'"'utils/download_predefined.php'"'"';" $@'
-alias pa_diff='php -r "require_once '"'"'utils/pan-diff.php'"'"';" $@'
-alias pa_config-size='php -r "require_once '"'"'utils/pan_config-size.php'"'"';" $@'
-alias pa_panos-xml-issue-detector='php -r "require_once '"'"'utils/panos-xml-issue-detector.php'"'"';" $@'
-alias pa_appid-enabler='php -r "require_once '"'"'utils/appid-enabler.php'"'"';" $@'
-
-alias pa_xml-op-json='php -r "require_once '"'"'utils/panXML_op_JSON.php'"'"';" $@'
-
-alias pa_bpa-generator='php -r "require_once '"'"'utils/bpa-generator.php'"'"';" $@'
-
-
-#OPTIMISATION NEEDED -> move to UTIL
-alias pa_gratuitous-arp='php -r "require_once '"'"'utils/sendGARP.php'"'"';" $@'
-alias pa_software-remove='php -r "require_once '"'"'utils/software-remove.php'"'"';" $@'
-alias pa_traffic-log='php -r "require_once '"'"'utils/traffic-log.php'"'"';" $@'
-alias pa_system-log='php -r "require_once '"'"'utils/system-log.php'"'"';" $@'
-
-alias pa_software-downloader='php -r "require_once '"'"'utils/pan_software_downloader.php'"'"';" $@'
-alias pa_software-preparation='php -r "require_once '"'"'utils/pan_software_download_preparation.php'"'"';" $@'
-
-alias pa_license='php -r "require_once '"'"'utils/pan_license.php'"'"';" $@'
-alias pa_config-download-all='php -r "require_once '"'"'utils/develop/download_config_all.php'"'"';" $@'
-##########################################################################################
-# until here all scripts above are deprecated and supported by pan-os-php type=
-##########################################################################################
-##########################################################################################
-
 
 
 #APPID TOOLBOX
@@ -105,19 +32,8 @@ alias pa_csv-import='php -r "require_once '"'"'utils/develop/csv-import.php'"'"'
 
 alias pa_ike='php -r "require_once '"'"'utils/develop/ike.php'"'"';" $@'
 
-alias pa_ssh-connector='php -r "require_once '"'"'utils/develop/ssh_connector.php'"'"';" $@'
 
 #license / software / commit-config / reset-config / get_user_info / sendGARP / software-remove / systemlog / traffic-log
 
-
-
-alias pa_config-commit='php -r "require_once '"'"'utils/develop/commit-config.php'"'"';" $@'
 alias pa_config-reset='php -r "require_once '"'"'utils/develop/reset-config.php'"'"';" $@'
 alias pa_get-system-user-info='php -r "require_once '"'"'utils/develop/pan_get_user_info.php'"'"';" $@'
-
-
-
-
-
-
-
