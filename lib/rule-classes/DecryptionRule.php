643102cca1d417c1fccca2457faf89b038942fcc
Sven Waschkut <swaschkut@paloaltonetworks.com>
2023-03-23 20:31:08 +0100

    type=rule ruletype=decryption | introduce category reading

--- lib/rule-classes/DecryptionRule.php
+++ lib/rule-classes/DecryptionRule.php
@@ -26,6 +26,17 @@ class DecryptionRule extends RuleWithUserID
 
     protected $_profile = null;
 
+    /** @var null|DOMElement */
+    protected $categoryroot = null;
+
+    /** @var string[] */
+    protected $_urlCategories = array();
+
+    /**
+     * @var UrlCategoryRuleContainer
+     */
+    public $urlCategories;
+
     /**
      * @param RuleStore $owner
      * @param bool $fromTemplateXML
@@ -58,6 +69,8 @@ class DecryptionRule extends RuleWithUserID
         $this->services = new ServiceRuleContainer($this);
         $this->services->name = 'service';
 
+        $this->urlCategories = new UrlCategoryRuleContainer($this);
+        $this->urlCategories->name = 'urlcategories';
 
         if( $fromTemplateXML )
         {
@@ -98,6 +111,38 @@ class DecryptionRule extends RuleWithUserID
                 $this->services->load_from_domxml($tmp);
         }
         // end of <service> zone extraction
+
+
+        //										//
+        // Begin <category> extraction			//
+        //										//
+        $tmp = DH::findFirstElement('category', $xml);
+        if( $tmp )
+        {
+            if( $tmp->hasChildNodes() )
+                $this->urlCategories->load_from_domxml($tmp);
+            else
+            {
+                //fixing own created code, introduced on 2020 Sep 18th, with creating <category/>
+                $tmp_member = DH::findFirstElementOrCreate('member', $tmp);
+                $tmp_member->textContent = 'any';
+            }
+        }
+
+        //
+        // Begin <category> extraction
+        //
+
+        /*     <category> <member>adult</member></category>      */
+
+        $this->categoryroot = DH::findFirstElement('category', $xml);
+        if( $this->categoryroot === FALSE )
+            $this->categoryroot = null;
+        else
+            $this->extract_category_from_domxml();
+
+        // End of <category>
+
 
         $profileXML = DH::findFirstElement('profile', $xml);
         if( $profileXML !== FALSE )
@@ -196,6 +241,19 @@ class DecryptionRule extends RuleWithUserID
             PH::$JSON_TMP['sub']['object'][$this->name()]['profile'] = $this->getDecryptionProfile();
         }
 
+        $text = $padding . "  URL Category: ";
+        if( !empty($this->_urlCategories) )
+        {
+            $text .= PH::list_to_string($this->_urlCategories) . "\n";
+            foreach( $this->_urlCategories as $tmp )
+                PH::$JSON_TMP['sub']['object'][$this->name()]['urlcategory'][] = $tmp;
+        }
+        else
+        {
+            $text .= "**ANY**";
+            PH::$JSON_TMP['sub']['object'][$this->name()]['urlcategory'][] = "**ANY**";
+        }
+        PH::print_stdout( $text );
 
         PH::print_stdout();
     }
@@ -217,6 +275,80 @@ class DecryptionRule extends RuleWithUserID
 
         $domNode = DH::findFirstElementOrCreate('profile', $this->xmlroot);
         DH::setDomNodeText($domNode, $newDecryptName);
+    }
+
+    protected function extract_category_from_domxml()
+    {
+        $xml = $this->categoryroot;
+
+        foreach( $xml->childNodes as $url_category )
+        {
+            if( $url_category->nodeType != XML_ELEMENT_NODE ) continue;
+
+            $value = $url_category->textContent;
+            if( strlen($value) < 1 )
+            {
+                mwarning('This rule has empty URL Category, please check your configuration file (corrupted?):', $url_category);
+                continue;
+            }
+            $this->_urlCategories[$value] = $value;
+            //Todo:
+            //search for URL and add to UrlCagegoriesContainer
+        }
+
+        if( isset($this->_urlCategories['any']) )
+        {
+            if( count($this->_urlCategories) != 1 )
+                mwarning('This security rule has URL category = ANY but it also have categories defined. ' .
+                    'Please check your configuration file (corrupted?). *ANY* will be assumed by this framework', $xml);
+            $this->_urlCategories = array();
+        }
+
+    }
+
+    public function urlCategories()
+    {
+        return $this->_urlCategories;
+    }
+
+    public function urlCategoriescount()
+    {
+        return count($this->_urlCategories);
+    }
+
+    public function urlCategoryIsAny()
+    {
+        return count($this->_urlCategories) == 0;
+    }
+
+    /**
+     * @param string $category
+     * @return bool return TRUE if this rule is using the category defined in $category
+     */
+    public function urlCategoriesHas($category)
+    {
+        return isset($this->_urlCategories[$category]);
+    }
+
+    /**
+     * enable or disabled logging at end
+     * @param bool $yes
+     * @return bool
+     */
+    public function setUrlCategories($category)
+    {
+        if( !isset( $this->_urlCategories[ $category ] ) )
+        {
+            $tmp = DH::findFirstElementOrCreate('category', $this->xmlroot);
+
+            $tmp_member = DH::findFirstElementOrCreate('member', $tmp);
+            $tmp_member->textContent = $category;
+
+            $this->_urlCategories[ $category ] = $category;
+
+            return TRUE;
+        }
+        return FALSE;
     }
 
     public function cleanForDestruction()
