6b017a170c0ec331a12742f691246f61f0a5541c
Sven Waschkut <swaschkut@paloaltonetworks.com>
2023-08-03 23:56:32 +0200

    type=dhcp | introduce actions=exporttoexcel:file.html

--- utils/common/actions-dhcp.php
+++ utils/common/actions-dhcp.php
@@ -89,3 +89,143 @@ DHCPCallContext::$supportedActions['dhcp-server-reservation-create'] = Array(
         'mac' => array('type' => 'string', 'default' => 'false'),
     ),
 );
+
+DHCPCallContext::$supportedActions['exportToExcel'] = array(
+    'name' => 'exportToExcel',
+    'MainFunction' => function (DHCPCallContext $context) {
+        $object = $context->object;
+        $context->objectList[] = $object;
+    },
+    'GlobalInitFunction' => function (DHCPCallContext $context) {
+        $context->objectList = array();
+    },
+    'GlobalFinishFunction' => function (DHCPCallContext $context) {
+        $args = &$context->arguments;
+        $filename = $args['filename'];
+
+        if( isset( $_SERVER['REQUEST_METHOD'] ) )
+            $filename = "project/html/".$filename;
+
+        $addWhereUsed = FALSE;
+        $addUsedInLocation = FALSE;
+
+        $optionalFields = &$context->arguments['additionalFields'];
+
+        if( isset($optionalFields['WhereUsed']) )
+            $addWhereUsed = TRUE;
+
+        if( isset($optionalFields['UsedInLocation']) )
+            $addUsedInLocation = TRUE;
+
+        $headers = '<th>ID</th><th>template</th><th>location</th><th>name</th>';
+        $headers .= '<th>Reservation</th>';
+
+        if( $addWhereUsed )
+            $headers .= '<th>where used</th>';
+        if( $addUsedInLocation )
+            $headers .= '<th>location used</th>';
+
+
+        $lines = '';
+
+        $count = 0;
+        if( isset($context->objectList) )
+        {
+            foreach( $context->objectList as $object )
+            {
+                $count++;
+
+                /** @var DHCP $object */
+                if( $count % 2 == 1 )
+                    $lines .= "<tr>\n";
+                else
+                    $lines .= "<tr bgcolor=\"#DDDDDD\">";
+
+                $lines .= $context->encloseFunction((string)$count);
+
+                if( $context->subSystem->isPanorama() )
+                {
+                    if( $object->owner->owner->owner->owner !== null && get_class($object->owner->owner->owner->owner) == "Template" )
+                    {
+                        $lines .= $context->encloseFunction($object->owner->owner->owner->owner->name());
+                        $lines .= $context->encloseFunction($object->owner->owner->name());
+                    }
+                    else
+                    {
+                        $lines .= $context->encloseFunction("");
+                        $lines .= $context->encloseFunction($object->owner->owner->name());
+                    }
+                }
+                else
+                {
+                    $lines .= $context->encloseFunction("");
+                    $lines .= $context->encloseFunction($object->owner->owner->name());
+                }
+
+
+                $lines .= $context->encloseFunction($object->name());
+
+                $tmpString = "";
+                foreach( $object->server_leases as $lease )
+                    $tmpString .= $lease['ip']." | ".$lease['mac'];
+                $lines .= $context->encloseFunction($tmpString);
+
+                if( $addWhereUsed )
+                {
+                    $refTextArray = array();
+                    foreach( $object->getReferences() as $ref )
+                        $refTextArray[] = $ref->_PANC_shortName();
+
+                    $lines .= $context->encloseFunction($refTextArray);
+                }
+                if( $addUsedInLocation )
+                {
+                    $refTextArray = array();
+                    foreach( $object->getReferences() as $ref )
+                    {
+                        $location = PH::getLocationString($object->owner);
+                        $refTextArray[$location] = $location;
+                    }
+
+                    $lines .= $context->encloseFunction($refTextArray);
+                }
+
+
+                $lines .= "</tr>\n";
+
+            }
+        }
+
+        $content = file_get_contents(dirname(__FILE__) . '/html/export-template.html');
+        $content = str_replace('%TableHeaders%', $headers, $content);
+
+        $content = str_replace('%lines%', $lines, $content);
+
+        $jscontent = file_get_contents(dirname(__FILE__) . '/html/jquery.min.js');
+        $jscontent .= "\n";
+        $jscontent .= file_get_contents(dirname(__FILE__) . '/html/jquery.stickytableheaders.min.js');
+        $jscontent .= "\n\$('table').stickyTableHeaders();\n";
+
+        $content = str_replace('%JSCONTENT%', $jscontent, $content);
+
+        file_put_contents($filename, $content);
+
+
+        file_put_contents($filename, $content);
+    },
+    'args' => array('filename' => array('type' => 'string', 'default' => '*nodefault*'),
+        'additionalFields' =>
+            array('type' => 'pipeSeparatedList',
+                'subtype' => 'string',
+                'default' => '*NONE*',
+                'choices' => array('WhereUsed', 'UsedInLocation', 'ResolveIP', 'NestedMembers'),
+                'help' =>
+                    "pipe(|) separated list of additional fields (ie: Arg1|Arg2|Arg3...) to include in the report. The following is available:\n" .
+                    "  - NestedMembers: lists all members, even the ones that may be included in nested groups\n" .
+                    "  - ResolveIP\n" .
+                    "  - UsedInLocation : list locations (vsys,dg,shared) where object is used\n" .
+                    "  - WhereUsed : list places where object is used (rules, groups ...)\n"
+            )
+    )
+
+);
