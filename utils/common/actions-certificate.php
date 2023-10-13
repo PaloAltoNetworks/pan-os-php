3c88adb66942f340e78e64ea72b90473b5dc8b97
Sven Waschkut <swaschkut@users.noreply.github.com>
2023-10-13 20:05:38 +0200

    type=certificate | introduce actions=exporttoexcel:CERT_file.html | 'filter=(expired < 90days)' | 'filter=(expired < today)')

--- utils/common/actions-certificate.php
+++ utils/common/actions-certificate.php
@@ -79,3 +79,104 @@ CertificateCallContext::$supportedActions['display'] = Array(
 "Name","Subject","Issuer","CA","Key","Expires","Status","Algorithm","Usage","Cloud Secret Name"
 "demo1234567","CN = test.test.de","CN = test.test.de","Yes","true","Oct 11 23:24:44 2024 GMT","valid","RSA","",""
  */
+
+CertificateCallContext::$supportedActions['exportToExcel'] = array(
+    'name' => 'exportToExcel',
+    'MainFunction' => function (CertificateCallContext $context) {
+        $object = $context->object;
+        $context->objectList[] = $object;
+    },
+    'GlobalInitFunction' => function (CertificateCallContext $context) {
+        $context->objectList = array();
+    },
+    'GlobalFinishFunction' => function (CertificateCallContext $context) {
+        $args = &$context->arguments;
+        $filename = $args['filename'];
+
+        if( isset( $_SERVER['REQUEST_METHOD'] ) )
+            $filename = "project/html/".$filename;
+
+
+
+        $headers = '<th>ID</th><th>template</th><th>location</th><th>name</th>';
+        $headers .= '<th>Algorithm</th><th>not Valid before</th><th>not Valid after</th>';
+
+
+
+        $lines = '';
+
+        $count = 0;
+        if( isset($context->objectList) )
+        {
+            foreach( $context->objectList as $object )
+            {
+                $count++;
+
+                /** @var DHCP $object */
+                if( $count % 2 == 1 )
+                    $lines .= "<tr>\n";
+                else
+                    $lines .= "<tr bgcolor=\"#DDDDDD\">";
+
+                $lines .= $context->encloseFunction((string)$count);
+
+                if( get_class($object->owner->owner) == "PANConf" )
+                {
+                    if( isset($object->owner->owner->owner) && $object->owner->owner->owner !== null && (get_class($object->owner->owner->owner) == "Template" || get_class($context->subSystem->owner) == "TemplateStack" ) )
+                    {
+                        $lines .= $context->encloseFunction($object->owner->owner->owner->name());
+                        $lines .= $context->encloseFunction($object->owner->owner->name());
+                    }
+                    else
+                    {
+                        $lines .= $context->encloseFunction("---");
+                        $lines .= $context->encloseFunction($object->owner->owner->name());
+                    }
+                }
+                elseif( isset($object->owner->owner) && $object->owner->owner !== null && (get_class($object->owner->owner) == "Template" || get_class($context->subSystem->owner) == "TemplateStack" ) )
+                {
+                    $lines .= $context->encloseFunction($object->owner->owner->name());
+                    $lines .= $context->encloseFunction($object->owner->name());
+                }
+
+
+                $lines .= $context->encloseFunction($object->name());
+
+                $algorithm = "";
+                if( $object->algorithm != null )
+                    $algorithm = $object->algorithm;
+                $lines .= $context->encloseFunction($algorithm);
+
+                $notValidbefore = "";
+                if( $object->notValidbefore != null )
+                    $notValidbefore = $object->notValidbefore;
+                $lines .= $context->encloseFunction($notValidbefore);
+
+                $notValidafter = "";
+                if( $object->notValidafter != null )
+                    $notValidafter = $object->notValidafter;
+                $lines .= $context->encloseFunction($notValidafter);
+
+
+                $lines .= "</tr>\n";
+
+            }
+        }
+
+        $content = file_get_contents(dirname(__FILE__) . '/html/export-template.html');
+        $content = str_replace('%TableHeaders%', $headers, $content);
+
+        $content = str_replace('%lines%', $lines, $content);
+
+        $jscontent = file_get_contents(dirname(__FILE__) . '/html/jquery.min.js');
+        $jscontent .= "\n";
+        $jscontent .= file_get_contents(dirname(__FILE__) . '/html/jquery.stickytableheaders.min.js');
+        $jscontent .= "\n\$('table').stickyTableHeaders();\n";
+
+        $content = str_replace('%JSCONTENT%', $jscontent, $content);
+
+        file_put_contents($filename, $content);
+
+    },
+    'args' => array('filename' => array('type' => 'string', 'default' => '*nodefault*'))
+);
